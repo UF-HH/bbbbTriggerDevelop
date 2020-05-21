1007caee0d7de73d20480035b7f4485fd2f8eb56
Giacomo Boldrini <g.boldrini2@campus.unimib.it>
2020-05-21 07:40:47 -0500

    Fix

--- ConfdbCustom/MenuModifier/AddPrimarySecondaryDatasets.py
+++ ConfdbCustom/MenuModifier/AddPrimarySecondaryDatasets.py
@@ -52,7 +52,7 @@ else:
 line = FindLine(menu_path, "_customInfo['inputFile' ]")
 if line != None:
     to_add = "_customInfo['inputFile' ]= ['@']"
-    ReplaceLine(self.menu, line, to_add)
+    ReplaceLine(menu_path, line, to_add)
 
 AddPrimarySecondaryPoolSource(menu_path, raw_data, aod_data, line)
 
