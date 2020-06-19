ddedd8a67595db9554901f3d72b94a43319898e4
Giacomo Boldrini <g.boldrini2@campus.unimib.it>
2020-06-19 06:56:22 -0500

    Base menu generator for timing

---
+++ ConfdbCustom/MenuModifier/BasicConfig_Timing.py
@@ -0,0 +1,88 @@
+#--------------------------------------------------------------------------------------------------------------------------------
+# author: Giacomo Boldrini
+# This script reproduces the full
+# HLT menu 11_0_0 with modules and
+# triggers from the analysis bbbb
+# Specific implementation for timing 
+# studies. Runs on vocms003
+#
+#python BasicConfig_Timing.py --menu=/online/collisions/2018/2e34/v3.6/HLT -gt=101X_dataRun2_HLT_v7 -out=myHLT.py -pr=TIMING -out=myHLT.py -nev=50000 -data --l1 L1Menu_Collisions2018_v2_1_0-d1_xml --tr=319941
+#
+#--------------------------------------------------------------------------------------------------------------------------------
+
+import os
+import sys
+import argparse
+import shutil
+from ModMenu import *
+from ModuleManager import ModMan
+from QueryForFiles import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-gt', '--globaltag', type=str, required=True, help="globaltag")
+parser.add_argument('-menu', '--menu', type=str, required=True, help="Menu config to be dumped")
+parser.add_argument('-pr', '--process', type=str, required=False, default="MyHLT", help="Process Name")
+parser.add_argument('-out', '--out', type=str, required=True, help="Output name of the menu created")
+parser.add_argument('-data', '--data', default=False, action='store_true', required=False, help="data or mc")
+parser.add_argument('-offline', '--offline', default=True, action='store_false', required=False, help="offline?")
+parser.add_argument('-type', '--type', type=str, required=False, default="GRun", help="GRun?")
+parser.add_argument('-paths', '--paths', type=str, required=False, help="Paths comma separated")
+parser.add_argument('-timing', '--timing', default=True, action='store_false', required=False, help="do you want timing modules?")
+parser.add_argument('-nev', '--nev', type=int, default=-1, required=False, help="Number of events to compute")
+parser.add_argument('-prescale', '--prescale', type=str, required=False, help="Prescale")
+parser.add_argument('-output', '--output', type=str, required=False, help="Outputmodules inside the menu")
+parser.add_argument('-input', '--input', type=str, required=False, help="Input files")
+parser.add_argument('-setup', '--setup', default="setup_cff", required=False, help="setup name")
+parser.add_argument('-l1', '--l1', type=str, required=False, help="l1 xml config")
+parser.add_argument('-TimingRun', '--tr', type=int, required=False, help="select a run to query files")
+
+args = parser.parse_args()
+
+#if a timing run is required we query for files
+if args.tr:
+    print("@[BeginQuery]: Query for timing files from run {}".format(args.tr))
+    run , pu, ps, files = QueryForTimingFiles(args.tr)
+    args.prescale = ps
+    print("@[EndQuery]: Queried for {} files".format(len(files)))
+
+print("@[BeginJob]: Initiating... ")
+
+to_os = "hltGetConfiguration {} --globaltag {} --process {} --full".format(args.menu, args.globaltag, args.process)
+
+if args.data: to_os += " --data"
+else: to_os += " --mc"
+if args.offline: to_os += " --offline"
+if args.type: to_os += " --type {}".format(args.type)
+if args.paths: to_os += " --paths {}".format(args.paths)
+if args.timing: to_os += " --timing"
+if args.nev: to_os += " --max-events {}".format(args.nev)
+if args.prescale: to_os += " --prescale {}".format(args.prescale)
+else: to_os += " --prescale none"
+if args.output: to_os += " --output {}".format(args.output)
+else: to_os += " --output none"
+if args.input: to_os += " --input {}".format(args.input)
+if args.l1: to_os += " --l1 {}".format(args.l1)
+
+to_os += " > {}".format(args.out)
+
+print("@[Info]: Dumping menu with proposed inputs: ")
+print(to_os)
+os.system(to_os)
+
+if os.path.isfile(args.out):
+    menu_path = args.out 
+
+
+print("@[Info]: Dumping Setup: ")
+to_os = "hltConfigFromDB --cff --configName {} --nopaths --services -PrescaleService,-EvFDaqDirector,-FastMonitoringService > {}.py".format(args.menu, args.setup)
+print(to_os)
+os.system(to_os)
+
+man = ModMan(menu_path)
+
+if args.tr:
+    print("@[Info]: Adding timing files as inputs... ")
+    man.AddDASQuery(files)
+
+print("@[EndJob]: Done")
